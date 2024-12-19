7da318e8fb53705d39a7d921d76d53af767342c6
Talia Frendl <taljbear@gmail.com>
2024-12-19 12:44:02 -0800

    React basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import Message from './Message';
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  return <div><Message /></div>
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
+
+// import ListGroup from "./components/ListGroup";
+
+// function App() {
+//   const items = [
+//     "New York",
+//     "San Francisco",
+//     "Los Angeles",
+//     "Chicago",
+//     "Miami",
+//   ];
+
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
