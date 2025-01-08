ff070e02da700a7ee856f0fb2242934690509c87
Talia Frendl <taljbear@gmail.com>
2025-01-07 17:52:14 -0800

    updated contact page spacing

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
     document.title = "Contact | Talia Joy Frendl";
   }, []);
   return (
-    <div className="Contact container text-center row justify-content-center">
+    <div className="Contact container-fluid d-flex justify-content-center align-items-center row text-center">
       <h1>Contact Me</h1>
       <p>I'll be glad to answer your questions!</p>
       <ContactForm />
