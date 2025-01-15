f889f45a8e237e23cef5f13860531c9c2702e89e
Talia Frendl <taljbear@gmail.com>
2025-01-14 16:59:16 -0800

    updated footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,40 +3,34 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <>
-      <footer className="text-center text-white py-3 mt-auto fixed-bottom">
-        <hr />
-        {/* <!-- Grid container --> */}
-        <div className="container ">
-          <div className="d-flex justify-content-between align-items-center mb-3">
-            <div>
-              {/* <!-- Linkedin --> */}
-              <a
-                className="btn btn-link btn-floating m-1 fs-4"
-                href="https://www.linkedin.com/in/talia-frendl/"
-                role="button"
-                data-mdb-ripple-color="dark"
-              >
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-              {/* <!-- Github --> */}
-              <a
-                className="btn btn-link btn-floating m-1 fs-4"
-                href="https://github.com/tfrendl"
-                role="button"
-                data-mdb-ripple-color="dark"
-              >
-                <i className="fab fa-github"></i>
-              </a>
-            </div>
-            {/* <!-- Copyright --> */}
-            <div className="text-center mb-3">© 2025 | Talia Frendl</div>
-          </div>
+    <footer className="text-center text-white py-3 mt-auto fixed-bottom">
+      <hr />
+      {/* Grid container */}
+      <div className="container d-flex justify-content-between align-items-center">
+        <div className="footer-icons d-flex justify-content-center w-100">
+          {/* LinkedIn Icon */}
+          <a
+            className="btn btn-link btn-floating m-1 fs-4"
+            href="https://www.linkedin.com/in/talia-frendl/"
+            role="button"
+            data-mdb-ripple-color="dark"
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          {/* GitHub Icon */}
+          <a
+            className="btn btn-link btn-floating m-1 fs-4"
+            href="https://github.com/tfrendl"
+            role="button"
+            data-mdb-ripple-color="dark"
+          >
+            <i className="fab fa-github"></i>
+          </a>
         </div>
-
-        {/* <!-- Copyright --> */}
-      </footer>
-    </>
+      </div>
+      {/* Copyright */}
+      <div className="text-center mb-3">© 2025 | Talia Frendl</div>
+    </footer>
   );
 };
 
